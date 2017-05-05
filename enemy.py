55f46facee0ebe6189c4759fb658ad07010af8f1
MaryKate Williams <mk21williams@gmail.com>
2017-05-05 18:06:01 -0400

    Gamespace and enemy modifications

--- enemy.py
+++ enemy.py
@@ -2,18 +2,47 @@ import pygame
 from pygame.locals import *
 
 class Enemy(pygame.sprite.Sprite):
-    def __init__(self, gs, speed):
+    def __init__(self, gs, speed, hp):
         
         super().__init__()
 
         self.gs = gs
-        
-        self.orig_image = pygame.image.load("/home/remote/mwilli37/project/paradigms_final_project/assets/enemy1.png")
-        self.image = self.orig_image
+
+        if hp > 1:
+            self.orig_image = pygame.image.load("/home/remote/mwilli37/project/paradigms_final_project/assets/enemy1.png")
+            scale = 0.20
+        else:
+            self.orig_image = pygame.image.load("/home/remote/mwilli37/project/paradigms_final_project/assets/enemy2.png")
+            scale = 0.10
+            
+        img_w,img_h = self.orig_image.get_size()
+        self.image = pygame.transform.scale(self.orig_image, (int(img_w*scale), int(img_h*scale))) 
         self.rect = self.image.get_rect()
 
         self.speed = speed
+        self.hp = hp
 
     def update(self):
 
+        # Move enemy downwards
         self.rect.y += self.speed
+
+        # Check to see if enemy is still within screen boundaries
+        screen_w, screen_h = self.gs.screen.get_size()
+        if self.rect.y > screen_h:
+            self.gs.enemy_list.remove(self)
+
+            
+        '''    
+        # Check to see if enemy has been hit
+        for b in self.gs.bullet_list:
+            col = pygame.sprite.collide_rect(b, self)
+            if col == True:
+                self.hp = self.hp - 1
+                self.gs.bullet_list.remove(b)
+
+        # Remove enemy from game if hit points has reached 0
+        if self.hp == 0:
+            self.gs.enemy_list.remove(self)
+        '''
+            
--- gamespace.py
+++ gamespace.py
@@ -3,17 +3,22 @@ import random, sys
 from pygame.locals import *
 
 from enemy import Enemy
+from player import Player
 
 class GameSpace:
     def main(self):
         pygame.init()
-        self.width = 500
+        self.width = 1000
         self.height = 500
         self.size = [self.width, self.height]
         self.black = 0,0,0
         self.screen = pygame.display.set_mode(self.size)
         self.background = pygame.Surface(self.screen.get_size())
 
+        self.player = Player(self)
+
+        self.bullet_list = pygame.sprite.Group()
+        
         self.enemy_list = pygame.sprite.Group()
         self.add_enemy_rate = 6
         self.enemy_count = 0
@@ -23,18 +28,37 @@ class GameSpace:
         while 1:
             self.clock.tick(60)
 
+            # Generate new enemies with random speed
             self.enemy_count +=1
             if self.enemy_count == self.add_enemy_rate:
                 self.enemy_count = 0
-                speed = random.randrange(1, 10)
-                enemy = Enemy(gs, speed)
+                speed = random.randrange(1, 5)
+                hp = random.randrange(1, 3)
+                enemy = Enemy(self, speed, hp)
                 enemy.rect.x = random.randrange(self.width)
                 enemy.rect.y = 0
-
                 self.enemy_list.add(enemy)
 
+            # Handle events
+            for event in pygame.event.get():
+                if event.type == KEYDOWN:
+                    self.player.move(event.key)
+                elif event.type == MOUSEBUTTONDOWN:
+                    self.player.fire = True
+                elif event.type == MOUSEBUTTONUP:
+                    self.player.fire = False
+                elif event.type == QUIT:
+                    pygame.display.quit()
+                
+            # Update game objects    
             self.enemy_list.update()
+            #self.player.update()
+            #self.bullet_list.update()
+
+            # Display current state of game objects
+            self.screen.fill(self.black)
             self.enemy_list.draw(self.screen)
+            self.screen.blit(self.player.image, self.player.rect)
             pygame.display.flip()
 
 if __name__ == '__main__':
